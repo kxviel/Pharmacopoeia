1ec673456cc13b066529d8add76b9ee915b9a597
Kevin D Goveas <64493268+kevkanae@users.noreply.github.com>
2020-11-05 21:56:56 +0530

    v2 + express js + ejs

--- index.js
+++ index.js
@@ -1,24 +1,45 @@
-const express = require('express');
-const https = require('https');
+const express = require("express");
+const https = require("https");
+const bodyP = require("body-parser");
+
 const app = express();
 
+app.set("view engine", "ejs");
+app.use(bodyP.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
 
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
 
-app.get("/",function(req,res){
-    const url = 'https://api.fda.gov/drug/ndc.json?api_key=S3DeKRRy8PtgRxfFGl5QbUlH0lxcAZ7QR2k8R9wH&search=generic_name:alitretinoin&limit=1';
-    https.get(url, function(response){
-        console.log(response);
+app.post("/display.html", function (req, res) {
+  var query = req.body.drug_name;
+  var type = "generic_name";
+  const apiKey = "S3DeKRRy8PtgRxfFGl5QbUlH0lxcAZ7QR2k8R9wH&";
+  var url =
+    "https://api.fda.gov/drug/ndc.json?api_key=" +
+    apiKey +
+    "search=" +
+    type +
+    ":" +
+    query +
+    "&limit=1";
+  https.get(url, function (response) {
+    console.log(response);
 
-        response.on('data',function(data){
-            const drug = JSON.parse(data);
-            const form = drug.results[0].dosage_form;
-            const genName = drug.results[0].generic_name;
-            res.send('The Name of the Drug is '+genName)
-        })
+    response.on("data", function (data) {
+      var drug = JSON.parse(data);
+      var form = drug.results[0].dosage_form;
+      var drug_name = drug.results[0].generic_name;
+      var brand_name = drug.results[0].brand_name;
+      var admin_route = drug.results[0].route;
+      var pharm_class = drug.results[0].pharm_class;
+
+      res.render('index',{drugName: drug_name, dosageForm: form, brandName: brand_name, route: admin_route, pharmClass: pharm_class});
     });
-    res.send("Server is OK Bruh")
-})
+  });
+});
 
-app.listen(3000,function(){
-    console.log('Server is Running');
-})
+app.listen(3000, function () {
+  console.log("Server is Running");
+});
--- jS/script.js
+++
@@ -1,6 +0,0 @@
-//form variable
-function myFunction() {
-  var x = document.getElementById("myForm");
-  var a = x[0].value;
-  sessionStorage.setItem("drug", a);
-}
