8a26aa1abadd702ce445bce424ce86caa2c11ca8
Kevin D Goveas <64493268+kevkanae@users.noreply.github.com>
2020-11-08 22:16:03 +0530

    fixed a LOT of errors

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//------------------------------------------------------------------//
+
 const express = require("express");
 const https = require("https");
 const bodyP = require("body-parser");
@@ -5,92 +7,107 @@ const mysql = require("mysql");
 
 const app = express();
 
+//------------------------------------------------------------------//
+
 app.set("view engine", "ejs");
 app.use(bodyP.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 
+//------------------------------------------------------------------//
+
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "crysis123",
+//   database: "mydb",
+// });
+
+//------------------------------------------------------------------//
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/login.html");
 });
-app.post("/home.html", function(req,res){
+
+//------------------------------------------------------------------//
+
+app.post("/home.html", function (req, res) {
   res.sendFile(__dirname + "/home.html");
 });
+
+//------------------------------------------------------------------//
 
 app.post("/display.html", function (req, res) {
   var query = req.body.drug_name;
   var type = "generic_name";
   const apiKey = "S3DeKRRy8PtgRxfFGl5QbUlH0lxcAZ7QR2k8R9wH&";
-  var url =
-    "https://api.fda.gov/drug/ndc.json?api_key=" +
+  var urlLabel =
+    "https://api.fda.gov/drug/label.json?api_key=" +
     apiKey +
-    "search=" +
+    "&search=openfda." +
     type +
     ":" +
     query +
-    "&limit=1";
-  https.get(url, function (response) {
-    console.log(response);
+    "&limit=2";
+  var urlNdc =
+    "https://api.fda.gov/drug/ndc.json?api_key=" +
+    apiKey +
+    "&search=" +
+    type +
+    ":" +
+    query +
+    "&limit=2";
 
-    response.on("data", function (data) {
-      var drug = JSON.parse(data);
-      var form = drug.results[0].dosage_form;
-      var drug_name = drug.results[0].generic_name;
-      var brand_name = drug.results[0].brand_name;
-      var admin_route = drug.results[0].route;
-      var pharm_class = drug.results[0].pharm_class;
-      var spl_id = drug.results[0].spl_id;
+  var drugName;
+  var dosageForm;
+  var brandName;
+  var route;
+  var pharmClass;
 
-      var con = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "crysis123",
-        database: "mydb",
+  function one() {
+    https.get(urlNdc, function (response1) {
+      console.log("HTTP Call 1 Done");
+
+      response1.on("data", function (data) {
+        //parse JSON data
+        //DB Table Name:ndc
+        drugName = JSON.parse(data).results[0].generic_name;
+        dosageForm = JSON.parse(data).results[0].dosage_form;
+        brandName = JSON.parse(data).results[0].brand_name;
+        route = JSON.parse(data).results[0].route;
+        pharmClass = JSON.parse(data).results[0].pharm_class;
       });
+    });
+  }
 
-      con.connect(function (err) {
-        if (err) throw err;
-        console.log("Connected!");
-
-        var sql = "SELECT * FROM drugs WHERE generic_name ='" + drug_name + "'";
-        con.query(sql, function (err, result) {
-          if (err) throw err;
-          console.log(result);
-          if (result.length != 0) {
-            res.render("index", {
-              drugName: drug_name,
-              dosageForm: form,
-              brandName: brand_name,
-              route: admin_route,
-              pharmClass: pharm_class,
-            });
-          } else {
-            var sql =
-              "INSERT INTO `drugs` (`spl_id`, `generic_name`, `dosage_form`) VALUES ('" +
-              spl_id +
-              "', '" +
-              drug_name +
-              "', '" +
-              form +
-              "')";
-            con.query(sql, function (err, result) {
-              res.render("index", {
-                drugName: drug_name,
-                dosageForm: form,
-                brandName: brand_name,
-                route: admin_route,
-                pharmClass: pharm_class,
-              });
-            });
-          }
+  function two() {
+    https.get(urlLabel, function (response2) {
+      console.log("HTTP Call 2 Done");
+      let body = "";
+      response2.on("data", function (data) {
+        body += data;
+      });
+      response2.on("end", function () {
+        var resp = JSON.parse(body);
+        res.render("index", {
+          drugName: drugName,
+          dosageForm: dosageForm,
+          brandName: brandName,
+          route: route,
+          pharmClass: pharmClass,
+          pp: resp.results[0].pharmacodynamics,
         });
       });
     });
-  });
+  }
+
+  one();
+  two();
 });
+
+//------------------------------------------------------------------//
 
 app.listen(3000, function () {
   console.log("Server is Running");
 });
 
-//(spl_id VARCHAR(40) PRIMARY KEY, generic_name VARCHAR(40), dosage_form VARCHAR(30))
+//------------------------------------------------------------------//
