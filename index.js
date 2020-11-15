78182fbf0e9f9574ea2c9c47a42f10741180a90c
Kevin D Goveas <64493268+kevkanae@users.noreply.github.com>
2020-11-15 19:49:51 +0530

    made server port dynamic

--- index.js
+++ index.js
@@ -198,7 +198,7 @@ function three(){
 
 //------------------------------------------------------------------//
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server is Running");
 });
 
