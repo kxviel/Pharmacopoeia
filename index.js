42232790778efcb65e7dd1f23b293ff5e788e88d
Kevin D Goveas <64493268+kevkanae@users.noreply.github.com>
2020-11-17 19:49:45 +0530

    home modified

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ app.post("/home.html", function (req, res) {
 //all the code written inside is to wrok out the data and pass it to display.html(index.ejs)
 app.post("/display.html", function (req, res) {
   var query = req.body.drug_name;
-  var type = "generic_name";
+  var type = "brand_name";
   const apiKey = "S3DeKRRy8PtgRxfFGl5QbUlH0lxcAZ7QR2k8R9wH&";
   var urlLabel =
     "https://api.fda.gov/drug/label.json?api_key=" +
@@ -131,6 +131,7 @@ app.post("/display.html", function (req, res) {
   var brandName;
   var route;
   var pharmClass;
+  var labelerName;
 
   function one() {
     https.get(urlNdc, function (response1) {
@@ -142,6 +143,7 @@ app.post("/display.html", function (req, res) {
         brandName = JSON.parse(data).results[0].brand_name;
         route = JSON.parse(data).results[0].route;
         pharmClass = JSON.parse(data).results[0].pharm_class;
+        labelerName = JSON.parse(data).results[0].labeler_name;
       });
     });
   }
@@ -153,26 +155,38 @@ app.post("/display.html", function (req, res) {
       response2.on("data", function (data) {
         body += data;
       });
+      var drugInteractions = JSON.parse(body).results[0].drug_interactions;
+      var description = JSON.parse(body).results[0].description;
+      var indicationanduse = JSON.parse(body).results[0].indications_and_usage;
+      var contraindications = JSON.parse(body).results[0].contraindications;
+      var pediatricuse = JSON.parse(body).results[0].pediatric_use;
+      var geriatricuse = JSON.parse(body).results[0].geriatric_use;
+      var infoforpatients = JSON.parse(body).results[0].information_for_patients;
+      var overdosage = JSON.parse(body).results[0].overdosage;
 
-function three(){
-  res.render("index", {
-    drugName: drugName == null ? 'Data N/A': drugName,
-    dosageForm: dosageForm == null ? 'Data N/A': dosageForm,
-    dosage: JSON.parse(body).results[0].dosage_forms_and_strengths == null ? 'Data N/A': JSON.parse(body).results[0].dosage_forms_and_strengths,
-    oD: JSON.parse(body).results[0].overdosage == null ? 'Data N/A': JSON.parse(body).results[0].overdosage,
-    brandName: brandName == null ? 'Data N/A': brandName,
-    route: route == null ? 'Data N/A': route,
-    pharmClass: pharmClass == null ? 'Data N/A': pharmClass,
-    pharm_dynamics: JSON.parse(body).results[0].pharmacodynamics == null ? 'Data N/A': JSON.parse(body).results[0].pharmacodynamics,
-    descr: JSON.parse(body).results[0].description == null ? 'Data N/A': JSON.parse(body).results[0].description,
-    pedo: JSON.parse(body).results[0].pediatric_use == null ? 'Data N/A': JSON.parse(body).results[0].pediatric_use,
-  });
-}
+      function three() {
+        res.render("index", {
+          drug_interactions: drugInteractions == null ? 'Data N/A' : drugInteractions,
+          indication_and_use: indicationanduse == null ? 'Data N/A' : indicationanduse,
+          contraindications: contraindications == null ? 'Data N/A' : contraindications,
+          info_for_patients: infoforpatients == null ? 'Data N/A' : infoforpatients,
+          geriatric_use: geriatricuse == null ? 'Data N/A' : geriatricuse,
+          drug_name: drugName == null ? 'Data N/A' : drugName,
+          dosage_form: dosageForm == null ? 'Data N/A' : dosageForm,
+          overdosage: overdosage == null ? 'Data N/A' : overdosage,
+          brand_name: brandName == null ? 'Data N/A' : brandName,
+          route: route == null ? 'Data N/A' : route,
+          pharm_class: pharmClass == null ? 'Data N/A' : pharmClass,
+          description: description == null ? 'Data N/A' : description,
+          pediatric_use: pediatricuse == null ? 'Data N/A' : pediatricuse,
+          labeler_name: labelerName == null ? 'Data N/A' : labelerName,
+        });
+      }
 
       response2.on("end", function () {
         var sql = "SELECT * FROM drugs WHERE DrugName ='" + drugName + "'";
         con.query(sql, function (err, result) {
-          if(err)throw err;
+          if (err) throw err;
           if (result.length == 1) { //if the drug name data exists in DB
             console.log('Drug Exists in DB');
             three();
@@ -181,7 +195,7 @@ function three(){
             console.log('Drug Doesnt Exist in DB');
             var sql = "INSERT INTO `drugs` (`DrugName`, `dosage_form`, `dosage`, `overDosage`, `brandName`, `administrationRoute`, `pharmClass`, `pharmDynamics`, `description`, `pediatricUse`) VALUES ('" + drugName + "', '" + dosageForm + "', '" + JSON.parse(body).results[0].dosage_forms_and_strengths + "', '" + JSON.parse(body).results[0].overdosage + "', '" + brandName + "', '" + route + "','" + pharmClass + "','" + JSON.parse(body).results[0].pharmacodynamics + "','" + JSON.parse(body).results[0].description + "','" + JSON.parse(body).results[0].pediatric_use + "');"
             con.query(sql, function (err, result) {
-              if(err)throw err;
+              if (err) throw err;
               three();
             });
           }
