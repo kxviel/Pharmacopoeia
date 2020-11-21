343b2417a6fc3d7188ba7349c19dbd4f31fa4c15
Kevin D Goveas <64493268+kevkanae@users.noreply.github.com>
2020-11-21 14:12:01 +0530

    Code Refactors

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-//------------------------------------------------------------------//
-
 const express = require("express");
 const https = require("https");
 const bodyP = require("body-parser");
@@ -8,13 +6,19 @@ const app = express();
 
 //------------------------------------------------------------------//
 
+//Register an EJS View Engine
 app.set("view engine", "ejs");
-app.use(bodyP.urlencoded({
-  extended: false
-}));
+//For Parsing Response Body
+app.use(
+  bodyP.urlencoded({
+    extended: false,
+  })
+);
+//For Initializing all Project Files in DIR
 app.use(express.static(__dirname));
 
 //------------------------------------------------------------------//
+//Connect To MySQL
 //use createPool instead of createConnection cause pool automatically opens and closes the connection
 let con = mysql.createPool({
   host: "localhost",
@@ -25,15 +29,15 @@ let con = mysql.createPool({
 
 //------------------------------------------------------------------//
 //gets the requests frm '/' i.e. login screen
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/login.html");
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/LogIn.html");
 });
 
 //------------------------------------------------------------------//
 //posts the requested content to home or wherever u want it but in simple ... to home.html
 //so all the requests i.e. filling forms, radio buttons,etc get posted to the home.html
 //aka, i can use /login.html or /signup.html values here for further coding
-app.post("/home.html", function (req, res) {
+app.post("/home", (req, res) => {
   //using these two above variables as my road dividers
   //if signinname == undefined it means user logged in
   //if loginpass == undefined it means user signed up
@@ -43,29 +47,27 @@ app.post("/home.html", function (req, res) {
     var passwordL = req.body.loginpass;
     var sqlu = "SELECT * FROM users WHERE username ='" + usernameL + "'";
     var sqlp = "SELECT * FROM users WHERE password ='" + passwordL + "'";
-    con.query(sqlu, function (err, result) {
+    con.query(sqlu, (err, result) => {
       if (err) throw err;
       //if username exists in DB
       if (result.length === 1) {
         //if user entered correct password
         console.log("User Exists in DB");
-        con.query(sqlp, function (err, result) {
+        con.query(sqlp, (err, result) => {
           if (err) throw err;
           if (result.length == 1) {
             //if correct pass continue
             console.log("Welcome " + usernameL);
-            res.sendFile(__dirname + "/home.html");
+            res.sendFile(__dirname + "/views/Home.html");
           } else {
             console.log("Wrong Password Bruh");
-            res.sendFile(__dirname + "/loginalert.html");
+            res.sendFile(__dirname + "/views/Error.html");
           }
         });
       } else {
-        res.sendFile(__dirname + "/loginalert.html");
+        res.sendFile(__dirname + "/views/Error.html");
       }
     });
-
-
   } else if (req.body.loginpass == undefined) {
     console.log("User Signed In ");
     var name = req.body.signinname;
@@ -76,7 +78,7 @@ app.post("/home.html", function (req, res) {
     //sql query to check if there is a user with the same email in the DB
     //If there is, then send err, else sign in successful
     var sql = "SELECT * FROM users WHERE email ='" + email + "'";
-    con.query(sql, function (err, result) {
+    con.query(sql, (err, result) => {
       if (err) throw err;
 
       if (result.length == 0) {
@@ -90,13 +92,13 @@ app.post("/home.html", function (req, res) {
           "', '" +
           password +
           "')";
-        con.query(insertSQL, function (err, result) {
+        con.query(insertSQL, (err, result) => {
           if (err) throw err;
-          console.log('Successful Added User');
-          res.sendFile(__dirname + "/home.html");
+          console.log("Successful Added User");
+          res.sendFile(__dirname + "/views/Home.html");
         });
       } else {
-        res.sendFile(__dirname + "/signupalert.html");
+        res.sendFile(__dirname + "/views/Error.html");
       }
     });
   }
@@ -105,7 +107,7 @@ app.post("/home.html", function (req, res) {
 //------------------------------------------------------------------//
 //posts the display html i.e. index.ejs using display.html as a template
 //all the code written inside is to work out the data and pass it to display.html(index.ejs)
-app.post("/display.html", function (req, res) {
+app.post("/display", (req, res) => {
   let query = req.body.drug_name;
   let type = "brand_name";
   const apiKey = "S3DeKRRy8PtgRxfFGl5QbUlH0lxcAZ7QR2k8R9wH&";
@@ -135,15 +137,15 @@ app.post("/display.html", function (req, res) {
   let productType;
 
   function one() {
-    https.get(urlNdc, function (response1) {
+    https.get(urlNdc, (response1) => {
       console.log("HTTP Call 1 Done");
 
-      response1.on("data", function (data) {
-        drugName = JSON.parse(data).results[0]['generic_name'];
-        dosageForm = JSON.parse(data).results[0]['dosage_form'];
-        brandName = JSON.parse(data).results[0]['brand_name'];
-        route = JSON.parse(data).results[0]['route'];
-        pharmClass = JSON.parse(data).results[0]['pharm_class'];
+      response1.on("data", (data) => {
+        drugName = JSON.parse(data).results[0]["generic_name"];
+        dosageForm = JSON.parse(data).results[0]["dosage_form"];
+        brandName = JSON.parse(data).results[0]["brand_name"];
+        route = JSON.parse(data).results[0]["route"];
+        pharmClass = JSON.parse(data).results[0]["pharm_class"];
         labelerName = JSON.parse(data).results[0]["labeler_name"];
         productType = JSON.parse(data).results[0]["product_type"];
       });
@@ -151,55 +153,105 @@ app.post("/display.html", function (req, res) {
   }
 
   function two() {
-    https.get(urlLabel, function (response2) {
+    https.get(urlLabel, (response2) => {
       console.log("HTTP Call 2 Done");
       let body = "";
-      response2.on("data", function (data) {
+      response2.on("data", (data) => {
         body += data;
       });
 
       function three() {
-        res.render("index", {
-          DrugName: drugName == null ? 'Data N/A' : drugName,
-          DosageForm: dosageForm == null ? 'Data N/A' : dosageForm,
-          OverDosage: JSON.parse(body).results[0]['overdosage'] == null ? 'Data N/A' : JSON.parse(body).results[0]['overdosage'],
-          BrandName: brandName == null ? 'Data N/A' : brandName,
-          AdminRoute: route == null ? 'Data N/A' : route,
-          PharmacologicalClass: pharmClass == null ? 'Data N/A' : pharmClass,
-          LabelerName: labelerName == null ? 'Data N/A' : labelerName,
-          Description: JSON.parse(body).results[0]['description'] == null ? 'Data N/A' : JSON.parse(body).results[0]['description'],
-          ProductType: productType == null ? 'Data N/A' : productType,
-          PediatricUse: JSON.parse(body).results[0]['pediatric_use'] == null ? 'Data N/A' : JSON.parse(body).results[0]['pediatric_use'],
-          DrugInteractions: JSON.parse(body).results[0]["drug_interactions"] == null ? 'Data N/A' : JSON.parse(body).results[0]["drug_interactions"],
-          Contraindications: JSON.parse(body).results[0]["contraindications"] == null ? 'Data N/A' : JSON.parse(body).results[0]["contraindications"],
-          InfoForPatients: JSON.parse(body).results[0]["information_for_patients"] == null ? 'Data N/A' : JSON.parse(body).results[0]["information_for_patients"],
-          GeriatricUse: JSON.parse(body).results[0]["geriatric_use"] == null ? 'Data N/A' : JSON.parse(body).results[0]["geriatric_use"],
-
+        res.render("Index", {
+          DrugName: drugName == null ? "Data N/A" : drugName,
+          DosageForm: dosageForm == null ? "Data N/A" : dosageForm,
+          OverDosage:
+            JSON.parse(body).results[0]["overdosage"] == null
+              ? "Data N/A"
+              : JSON.parse(body).results[0]["overdosage"],
+          BrandName: brandName == null ? "Data N/A" : brandName,
+          AdminRoute: route == null ? "Data N/A" : route,
+          PharmacologicalClass: pharmClass == null ? "Data N/A" : pharmClass,
+          LabelerName: labelerName == null ? "Data N/A" : labelerName,
+          Description:
+            JSON.parse(body).results[0]["description"] == null
+              ? "Data N/A"
+              : JSON.parse(body).results[0]["description"],
+          ProductType: productType == null ? "Data N/A" : productType,
+          PediatricUse:
+            JSON.parse(body).results[0]["pediatric_use"] == null
+              ? "Data N/A"
+              : JSON.parse(body).results[0]["pediatric_use"],
+          DrugInteractions:
+            JSON.parse(body).results[0]["drug_interactions"] == null
+              ? "Data N/A"
+              : JSON.parse(body).results[0]["drug_interactions"],
+          Contraindications:
+            JSON.parse(body).results[0]["contraindications"] == null
+              ? "Data N/A"
+              : JSON.parse(body).results[0]["contraindications"],
+          InfoForPatients:
+            JSON.parse(body).results[0]["information_for_patients"] == null
+              ? "Data N/A"
+              : JSON.parse(body).results[0]["information_for_patients"],
+          GeriatricUse:
+            JSON.parse(body).results[0]["geriatric_use"] == null
+              ? "Data N/A"
+              : JSON.parse(body).results[0]["geriatric_use"],
         });
       }
 
-      response2.on("end", function () {
+      response2.on("end", () => {
         let sql = "SELECT * FROM drugs WHERE DrugName ='" + drugName + "'";
-        con.query(sql, function (err, result) {
+        con.query(sql, (err, result) => {
           if (err) throw err;
-          if (result.length === 1) { //if the drug name data exists in DB
-            console.log('Drug Exists in DB');
+          if (result.length === 1) {
+            //if the drug name data exists in DB
+            console.log("Drug Exists in DB");
             three();
-            console.log('Display Rendered');
-          } else { //if drug name doesnt exist in DB
+            console.log("Display Rendered");
+          } else {
+            //if drug name doesnt exist in DB
             //insert
-            console.log('Drug Doesnt Exist in DB');
-            let sql = "INSERT INTO `drugs` (`DrugName`, `DosageForm`, `OverDosage`, `BrandName`, `AdminRoute`, `PharmacologicalClass`, `LabelerName`, `Description`, `ProductType`, `PediatricUse`, `DrugInteractions`, `Contraindications`, `InfoForPatients`, `GeriatricUse`) VALUES ('" + drugName + "', '" + dosageForm + "', '" + JSON.parse(body).results[0]['overdosage'] + "', '" + brandName + "', '" + route + "', '" + pharmClass + "','" + labelerName + "','" + JSON.parse(body).results[0]['description'] + "','" + productType + "','" + JSON.parse(body).results[0]['pediatric_use'] + "','" + JSON.parse(body).results[0]["drug_interactions"] + "','" + JSON.parse(body).results[0]["contraindications"] + "','" + JSON.parse(body).results[0]["information_for_patients"] + "','" + JSON.parse(body).results[0]["geriatric_use"] + "');";
+            console.log("Drug Doesnt Exist in DB");
+            let sql =
+              "INSERT INTO `drugs` (`DrugName`, `DosageForm`, `OverDosage`, `BrandName`, `AdminRoute`, `PharmacologicalClass`, `LabelerName`, `Description`, `ProductType`, `PediatricUse`, `DrugInteractions`, `Contraindications`, `InfoForPatients`, `GeriatricUse`) VALUES ('" +
+              drugName +
+              "', '" +
+              dosageForm +
+              "', '" +
+              JSON.parse(body).results[0]["overdosage"] +
+              "', '" +
+              brandName +
+              "', '" +
+              route +
+              "', '" +
+              pharmClass +
+              "','" +
+              labelerName +
+              "','" +
+              JSON.parse(body).results[0]["description"] +
+              "','" +
+              productType +
+              "','" +
+              JSON.parse(body).results[0]["pediatric_use"] +
+              "','" +
+              JSON.parse(body).results[0]["drug_interactions"] +
+              "','" +
+              JSON.parse(body).results[0]["contraindications"] +
+              "','" +
+              JSON.parse(body).results[0]["information_for_patients"] +
+              "','" +
+              JSON.parse(body).results[0]["geriatric_use"] +
+              "');";
 
-            con.query(sql, function (err, result) {
+            con.query(sql, (err, result) => {
               if (err) throw err;
-              console.log('Drug Data Entered into DB');
+              console.log("Drug Data Entered into DB");
               three();
-              console.log('Display Rendered');
+              console.log("Display Rendered");
             });
           }
         });
-
       });
     });
   }
@@ -209,9 +261,16 @@ app.post("/display.html", function (req, res) {
   two();
 });
 
+app.get("/map", (req, res) => {
+  let hey = "my abab";
+  res.render("Map", {
+    text: hey,
+  });
+});
+
 //------------------------------------------------------------------//
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 777, () => {
   console.log("Server is Running");
 });
 
