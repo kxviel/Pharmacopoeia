2b08954ec6271ff5699acafcc62941fda9630702
Kevin D Goveas <64493268+kevkanae@users.noreply.github.com>
2020-11-26 12:08:25 +0530

    minor changes

--- index.js
+++ index.js
@@ -37,113 +37,6 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/LogIn.html");
 });
 
-//All Get Requests to '/map'
-app.get("/map", (req, resmain) => {
-  let myLocation = req.query.mylocation;
-
-  //url Variables
-  const clientID = "BFL2Y52PUJAONSK3UICXZAH3QC2JZ2UJWJBQIISYVWB0MGVN";
-  const clientSecret = "DKAK11LMZZHILZWA0WSV2W4DV0UBEGU03CBVZ5FCSH0XAQEQ";
-  const query = "pharmacy";
-  const date = "20201120";
-
-  //Get Lat and Long from City Name Entered
-  let latitude;
-  let longitude;
-  let myBody;
-  let options = {
-    provider: "openstreetmap",
-  };
-  let geoCoder = geo(options);
-
-  async function getLatLang() {
-    myBody = await geoCoder.geocode(myLocation);
-  }
-
-  function getMapURL() {
-    latitude = myBody[0]["latitude"];
-    longitude = myBody[0]["longitude"];
-    mapURL =
-      "https://api.foursquare.com/v2/venues/search?ll=" +
-      latitude +
-      "," +
-      longitude +
-      "&query=" +
-      query +
-      "&radius=10000&client_id=" +
-      clientID +
-      "&client_secret=" +
-      clientSecret +
-      "&v=" +
-      date;
-    var location1, meters1, city1;
-    var location2, meters2, city2;
-    var location3, meters3, city3;
-    var location4, meters4, city4;
-    https
-      .get(mapURL, function (res) {
-        var json = "";
-        res.on("data", function (chunk) {
-          json += chunk;
-        });
-        res.on("end", function () {
-          if (res.statusCode === 200) {
-            try {
-              var i;
-              var a = [],
-                b = [],
-                c = [];
-              for (i = 0; i <= 3; i++) {
-                a[i] = JSON.parse(json).response.venues[i].name;
-                b[i] = JSON.parse(json).response.venues[i].location.distance;
-                c[i] = JSON.parse(json).response.venues[i].location.city;
-              }
-              location1 = a[0];
-              location2 = a[1];
-              location3 = a[2];
-              location4 = a[3];
-              meters1 = b[0];
-              meters2 = b[1];
-              meters3 = b[2];
-              meters4 = b[3];
-              city1 = c[0];
-              city2 = c[1];
-              city3 = c[2];
-              city4 = c[3];
-
-              resmain.render("Map", {
-                Heading: "Pharmacies Near Me",
-                loc1: location1 == null ? "No Shops Nearby" : location1,
-                loc2: location2 == null ? "No Shops Nearby" : location2,
-                loc3: location3 == null ? "No Shops Nearby" : location3,
-                loc4: location4 == null ? "No Shops Nearby" : location4,
-                met1: meters1 == null ? "N/A" : meters1,
-                met2: meters2 == null ? "N/A" : meters2,
-                met3: meters3 == null ? "N/A" : meters3,
-                met4: meters4 == null ? "N/A" : meters4,
-                cit1: city1 == null ? "N/A" : city1,
-                cit2: city2 == null ? "N/A" : city2,
-                cit3: city3 == null ? "N/A" : city3,
-                cit4: city4 == null ? "N/A" : city4,
-              });
-            } catch (e) {
-              console.log(e, "Error parsing JSON!");
-            }
-          } else {
-            console.log("Status:", res.statusCode);
-          }
-        });
-      })
-      .on("error", function (err) {
-        console.log("Error:", err);
-      });
-  }
-
-  //Resolve Async Function
-  getLatLang();
-  setTimeout(getMapURL, 3000);
-});
-
 //------------------------------------------------------------------//
 
 //All Post Requests to '/home'
@@ -374,11 +267,167 @@ app.post("/display", (req, res) => {
 
 //------------------------------------------------------------------//
 
+//All Get Requests to '/map'
+app.get("/map", (req, resmain) => {
+  //Get form data with city name
+  let myLocation = req.query.mylocation;
+
+  //url Variables
+  const clientID = "BFL2Y52PUJAONSK3UICXZAH3QC2JZ2UJWJBQIISYVWB0MGVN";
+  const clientSecret = "DKAK11LMZZHILZWA0WSV2W4DV0UBEGU03CBVZ5FCSH0XAQEQ";
+  const query = "pharmacy";
+  const date = "20201120";
+
+  //Get Lat and Long from City Name Entered
+  let latitude;
+  let longitude;
+  let myBody;
+  var a = [],
+    b = [],
+    c = [],
+    d = [],
+    e = [];
+  let options = {
+    provider: "openstreetmap",
+  };
+  let geoCoder = geo(options);
+
+  async function getLatLang() {
+    myBody = await geoCoder.geocode(myLocation);
+  }
+
+  function getMapURL() {
+    latitude = myBody[0]["latitude"];
+    longitude = myBody[0]["longitude"];
+    mapURL =
+      "https://api.foursquare.com/v2/venues/search?ll=" +
+      latitude +
+      "," +
+      longitude +
+      "&query=" +
+      query +
+      "&radius=10000&client_id=" +
+      clientID +
+      "&client_secret=" +
+      clientSecret +
+      "&v=" +
+      date;
+    var location1, meters1, city1;
+    var location2, meters2, city2;
+    var location3, meters3, city3;
+    var location4, meters4, city4;
+    var location5, meters5, city5;
+    //Random Phone Number
+    function phoneNumber() {
+      let randomNumber = Math.floor("9" + Math.random() * 900000000);
+      let phoneNumber = "+91 " + `${randomNumber}`;
+      return phoneNumber;
+    }
+
+    //Availability
+    function availability() {
+      var yesNo = ["✔", "❌"];
+      var rand = Math.floor(Math.random() * yesNo.length);
+      let availability = yesNo[rand];
+      return availability;
+    }
+
+    //Parse JSON and Render
+    https
+      .get(mapURL, function (res) {
+        var json = "";
+        res.on("data", function (chunk) {
+          json += chunk;
+        });
+        res.on("end", function () {
+          if (res.statusCode === 200) {
+            try {
+              var i;
+
+              for (i = 0; i <= 4; i++) {
+                a[i] = JSON.parse(json).response.venues[i].name;
+                b[i] = JSON.parse(json).response.venues[i].location.distance;
+                c[i] = JSON.parse(json).response.venues[i].location.city;
+                d[i] = availability();
+                e[i] = phoneNumber();
+              }
+
+              //Insert Data Into Table
+              for (i = 0; i <= 4; i++) {
+                let sql =
+                  "INSERT INTO `stores` (`StoreNames`,`SearchedLocation`,`StoreDistance`) VALUES ('" +
+                  a[i] +
+                  "','" +
+                  myLocation +
+                  "','" +
+                  b[i] +
+                  "');";
+                con.query(sql, (err, result) => {
+                  if (err) throw err;
+                });
+              }
+
+              resmain.render("Map", {
+                Heading: "Pharmacies Near Me",
+                loc1: a[0] ?? "No Shops Nearby",
+                loc2: a[1] ?? "No Shops Nearby",
+                loc3: a[2] ?? "No Shops Nearby",
+                loc4: a[3] ?? "No Shops Nearby",
+                loc5: a[4] ?? "No Shops Nearby",
+                met1: b[0] ?? "N/A",
+                met2: b[1] ?? "N/A",
+                met3: b[2] ?? "N/A",
+                met4: b[3] ?? "N/A",
+                met5: b[4] ?? "N/A",
+                cit1: c[0] ?? "N/A",
+                cit2: c[1] ?? "N/A",
+                cit3: c[2] ?? "N/A",
+                cit4: c[3] ?? "N/A",
+                cit5: c[4] ?? "N/A",
+                phone1: e[0],
+                avail1: d[0],
+                phone2: e[1],
+                avail2: d[1],
+                phone3: e[2],
+                avail3: d[2],
+                phone4: e[3],
+                avail4: d[3],
+                phone5: e[4],
+                avail5: d[4],
+              });
+            } catch (exc) {
+              console.log(exc, "Error parsing JSON!");
+            }
+          } else {
+            console.log("Status:", res.statusCode);
+          }
+        });
+      })
+      .on("error", function (err) {
+        console.log("Error:", err);
+      });
+  }
+
+  //Resolve Async Function
+  getLatLang();
+  setTimeout(getMapURL, 3000);
+});
+
+//------------------------------------------------------------------//
+
 //Server Launch at Port 777
 app.listen(process.env.PORT || 777, () => {
   console.log("Server is Running at localhost:777");
 });
 
 //------------------------------------------------------------------//
-//CREATE TABLE `users` (`name` varchar(100) ,`email` varchar(255) ,`username` varchar(255),`password` varchar(255));
-//CREATE TABLE `drugs` (drug_id int NOT NULL AUTO_INCREMENT, `DrugName` varchar(255) NOT NULL, `DosageForm` TEXT, `OverDosage` TEXT, `BrandName` TEXT, `AdminRoute` varchar(255), `PharmacologicalClass` TEXT, `LabelerName` TEXT, `Description` TEXT, `ProductType` TEXT, `PediatricUse` TEXT, `DrugInteractions` TEXT, `Contraindications` TEXT, `InfoForPatients` TEXT, `GeriatricUse` TEXT, PRIMARY KEY(drug_id));
+// CREATE TABLE `users` (`name` varchar(100) ,`email` varchar(255) ,`username` varchar(255),
+// `password` varchar(255),PRIMARY KEY(username));
+
+// CREATE TABLE `drugs` (drug_id int NOT NULL AUTO_INCREMENT, `DrugName` varchar(255) NOT NULL,
+// `DosageForm` TEXT, `OverDosage` TEXT, `BrandName` TEXT, `AdminRoute` varchar(255),
+// `PharmacologicalClass` TEXT, `LabelerName` TEXT, `Description` TEXT, `ProductType` TEXT,
+// `PediatricUse` TEXT, `DrugInteractions` TEXT, `Contraindications` TEXT, `InfoForPatients` TEXT,
+// `GeriatricUse` TEXT, PRIMARY KEY(drug_id));
+
+// CREATE TABLE `stores` (`store_id` int NOT NULL AUTO_INCREMENT, `StoreNames` varchar(100),`SearchedLocation` varchar(100),`StoreDistance` int,PRIMARY KEY(store_id));
